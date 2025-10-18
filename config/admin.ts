74c8c30c2d83bc3916a5dfec7e08ff0d92f73eba
KHRIISTOFOR <khristofor@example.com>
2025-10-18 15:09:15 +0500

    Fix admin configuration - remove time settings

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,6 @@
 export default ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
-    options: {
-      expiresIn: '7d',
-    },
-    sessions: {
-      maxRefreshTokenLifespan: '7d',
-      maxSessionLifespan: '7d',
-    },
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
@@ -21,8 +14,8 @@ export default ({ env }) => ({
     encryptionKey: env('ENCRYPTION_KEY'),
   },
   flags: {
-    nps: env.bool('FLAG_NPS', false),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', false),
+    nps: false,
+    promoteEE: false,
   },
   url: env('PUBLIC_URL', 'http://localhost:1337'),
   serveAdminPanel: true,
