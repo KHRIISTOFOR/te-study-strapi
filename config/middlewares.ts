53894586a02664b681dfef3577ff8af2a3cb5795
KHRIISTOFOR <khristofor@example.com>
2025-10-18 12:20:54 +0500

    Initial Strapi setup for TE.STUDY

---
+++ config/middlewares.ts
@@ -0,0 +1,17 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://te-study.com', 'http://localhost:3001', 'http://localhost:3000', 'https://your-strapi-app.railway.app']
+    }
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
